d1ea23614ca442461e2b50d8d4bab29430a2983c
devansh-kc <devansh.kc96@gmail.com>
2024-06-07 17:49:37 +0530

    added changes

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -21,18 +21,22 @@ export const Work = [
   {
     id: 1,
     title: "Music academy frontend using Nextjs",
-    Link: "https://github.com/devansh-kc/Music-Academy-next-js",
     description:
-      "So I made this project by using next js because i want to learn how frontend dev works in next js. ",
+      "The Music Academy project is a comprehensive platform designed to connect music enthusiasts with professional instructors. It facilitates easy scheduling of music lessons, showcases instructor profiles, and provides resources for learners to enhance their skills.",
+    bulletPoints:
+      "Resource Library: Front-end design for accessing learning materials like video tutorials and sheet music.Responsive Design: Ensures seamless user experience across all devices.",
     header: academy,
+    TecUsed:
+      "<li>Next.js: Framework for building server-rendered React applications.</li> <li>TypeScript: Adds type safety to JavaScript, ensuring more reliable code.</li> <li>Tailwind CSS: Utility-first CSS framework for rapid UI development.</li>",
   },
   {
     id: 2,
     title: "Blog App using react and Appwrite",
     Link: "https://github.com/devansh-kc/blog-App",
-    description:
-      "I have used react,redux,react-hook-form and appwrite to make this  project.",
+    description:"The Blog App is a full-stack web application designed to facilitate the creation, management, and display of blog posts. It provides a user-friendly interface for authors to write and publish their content, and for readers to browse and engage with the posts. This project demonstrates a comprehensive use of modern web development technologies and best practices, focusing on performance, scalability, and user experience",
+    bulletPoints:"User Authentication: Secure user authentication and authorization using Appwrite, ensuring that only authenticated users can create, edit, or delete blog posts.CRUD Operations: Full CRUD (Create, Read, Update, Delete) functionality for blog posts, allowing users to manage their content effortlessly.Rich Text Editor: Integrated rich text editor for composing blog posts with formatting options, enhancing the writing experience for authors.Responsive Design: Fully responsive layout, ensuring optimal viewing and interaction experience across various devices and screen sizes.SEO Optimization: Implemented SEO best practices to improve the visibility and search engine ranking of blog posts.Performance Optimization: Focused on performance improvements to ensure fast load times and smooth interactions.Database Integration: Utilized Appwrite's database services for data storage, providing a robust and scalable solution for managing blog posts and user data.Error Handling: Comprehensive error handling and validation using Zod, ensuring data integrity and providing meaningful feedback to users.",
     header: Blog,
+    TecUsed:"<li>Frontend: React.js</li><li>Backend: Appwrite </li><li>Database: Appwrite Database</li><li>Authentication: Appwrite Auth</li><li>Styling: CSS Modules, Tailwind CSS</li><li>Hosting: Vercel</li>"
   },
   {
     id: 3,
@@ -50,6 +54,14 @@ export const Work = [
     description:
       "So I have made this authentication app using next js , because i want to learn how authentication works in next js . ",
     header: NextAuth,
+  },
+  {
+    id: 5,
+    title: "anonyms messaging app",
+    Link: "https://github.com/devansh-kc/next-auth",
+    header: NextAuth,
+    description:
+      "So I have made this authentication app using next js , because i want to learn how authentication works in next js . ",
   },
   // {
   //   title: "The Pursuit of Knowledge",
